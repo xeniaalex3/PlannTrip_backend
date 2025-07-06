5a8604959534c275802f2577048638059d8ea1eb
[xeniaalex3] <[xeniaalex2@gmail.com]>
2025-07-06 12:59:15 +0200

    ✨ : update documentation and integrate Scalar API reference

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,29 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const options = new DocumentBuilder()
     .setTitle('PlannTrip documentatio API')
     .setDescription('Documentation')
     .setVersion('1.0')
     .addServer('http://localhost:3000/', 'Local environment')
-    .addServer('https://staging.yourapi.com/', 'Staging')
-    .addServer('https://production.yourapi.com/', 'Production')
     .addTag('Your API Tag')
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
+
+  app.use(
+    '/reference',
+    apiReference({
+      content: document,
+      theme: 'purple',
+    }),
+  );
 
   app.enableCors();
 
