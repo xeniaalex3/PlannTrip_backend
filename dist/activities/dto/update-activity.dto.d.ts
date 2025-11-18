549def056b830b9f8b51f91895eb0a52a2ad9b0d
[xeniaalex3] <[xeniaalex2@gmail.com]>
2025-11-18 17:21:25 +0100

    apply gitignore cleanup

--- dist/activities/dto/update-activity.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateActivityDto } from './create-activity.dto';
-declare const UpdateActivityDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateActivityDto>>;
-export declare class UpdateActivityDto extends UpdateActivityDto_base {
-}
-export {};
