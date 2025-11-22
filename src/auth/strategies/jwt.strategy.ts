5ed89fb8fafb11a8f943e5565d64b07ecfddac25
[xeniaalex3] <[xeniaalex2@gmail.com]>
2025-11-22 11:48:13 +0100

    ✨ : enhance AuthRefreshService and JwtStrategy with type definitions for payloads, improving type safety and code clarity

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -3,6 +3,11 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
 
+interface JwtPayload {
+  sub: string;
+  email: string;
+}
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private configService: ConfigService) {
@@ -13,7 +18,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
+  async validate(payload: JwtPayload) {
     return { id: payload.sub, email: payload.email };
   }
 }
