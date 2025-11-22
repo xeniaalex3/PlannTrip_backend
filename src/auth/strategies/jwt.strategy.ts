fbc4671698dc5f5e704ced7f0b6514c1aa974714
[xeniaalex3] <[xeniaalex2@gmail.com]>
2025-11-22 12:48:55 +0100

    🔧 : configure global environment file path in ConfigModule and add logging for JWT_SECRET retrieval

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -12,6 +12,7 @@ interface JwtPayload {
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private configService: ConfigService) {
     const jwtSecret = configService.get<string>('JWT_SECRET');
+    console.log('JWT_SECRET:', jwtSecret);
     if (!jwtSecret) {
       throw new Error('JWT_SECRET must be defined in .env');
     }
@@ -20,7 +21,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       ignoreExpiration: false,
       secretOrKey: jwtSecret,
     });
-    console.log('JWT_SECRET LOADED:', configService.get('JWT_SECRET'));
   }
 
   async validate(payload: JwtPayload) {
